241a9992ba75670ce77f50dd1c3a80867c3ac5ae
Julia Morozova <yuli.sleptsova@gmail.com>
2022-01-03 21:16:08 +0300

    add different variant of type

--- src/components/TodoListHeader/TodoListHeader.tsx
+++ src/components/TodoListHeader/TodoListHeader.tsx
@@ -3,8 +3,8 @@ import React from "react";
 type TodoListHeaderProps = {
     title: string
 }
-const TodoListHeader = (props: TodoListHeaderProps) => {
-    return <h3>{props.title}</h3>
+const TodoListHeader: React.FC<TodoListHeaderProps> = ({title}: TodoListHeaderProps) => {
+    return <h3>{title}</h3>
 }
 
 export default TodoListHeader;
