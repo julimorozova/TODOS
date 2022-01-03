f1aeae8eaef7c90bbfb8a13b2317b075bb70bcae
user <user@ADM-MSK-0509.local>
2022-01-03 19:37:58 +0300

    project initial, add a TodoList component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./TodoList";
+
+function App() {
+    return (
+        <div className="App">
+            <TodoList title="What to learn" />
+            <TodoList title="Songs"/>
+            <TodoList title="Books"/>
+        </div>
+    );
+}
+export default App;
